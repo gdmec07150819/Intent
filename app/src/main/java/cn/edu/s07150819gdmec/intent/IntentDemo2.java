415d72ac8a570f4791a3f9ceb762eaadd2595ee2
gdmec07150819 <844298721@qq.com>
2016-10-31 13:20:43 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/s07150819gdmec/intent/IntentDemo2.java
@@ -0,0 +1,27 @@
+package cn.edu.s07150819gdmec.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/10/30.
+ */
+public class IntentDemo2 extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo2);
+    }
+    public void upper(View v){
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        final String value = bundle.getString("value").toUpperCase();
+        Intent intentresult = new Intent();
+        intentresult.putExtra("result",value);
+        setResult(RESULT_OK,intentresult);
+        finish();
+    }
+}
---
+++ app/src/main/java/cn/edu/s07150819gdmec/intent/IntentDemo3.java
@@ -0,0 +1,7 @@
+package cn.edu.s07150819gdmec.intent;
+
+/**
+ * Created by Administrator on 2016/10/30.
+ */
+public class IntentDemo3 {
+}
